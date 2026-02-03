73d70afba852a4746a7f3d271ed4dfe9feea19e2
PirarbQT <johnny.staffsign@gmail.com>
2026-02-03 11:44:40 +0700

    Use DATABASE_URL for Postgres

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -2,11 +2,8 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "school_db",      // <-- ชื่อ DB ของคุณ
-  password: "1234",      // <-- รหัส PostgreSQL ของคุณ
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 export default pool;
