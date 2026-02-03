a33c28049a8a31e72e08ea549afff92a710c23ba
PirarbQT <johnny.staffsign@gmail.com>
2026-02-03 12:50:29 +0700

    Update frontend

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -7,3 +7,9 @@ const pool = new Pool({
 });
 
 export default pool;
+
+
+/*const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+});*/
